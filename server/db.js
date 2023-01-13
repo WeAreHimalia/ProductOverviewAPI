379de3f670b7d854c0d7b0307488f95a4dd8cb32
Calvin Goodale <calvin.goodale@gmail.com>
2023-01-12 22:52:59 -0500

    Mongoose and schema skeleton

---
+++ server/db.js
@@ -0,0 +1,33 @@
+// Import the mongoose module
+const mongoose = require("mongoose");
+
+// Set up default mongoose connection
+const mongoDB = "mongodb://127.0.0.1/sdc";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// Define a schema
+const Schema = mongoose.Schema;
+
+const ProductsSchema = new Schema({
+  name: String,
+  binary: Buffer,
+  living: Boolean,
+  updated: { type: Date, default: Date.now() },
+  age: { type: Number, min: 18, max: 65, required: true },
+  mixed: Schema.Types.Mixed,
+  _someId: Schema.Types.ObjectId,
+  array: [],
+  ofString: [String], // You can also have an array of each of the other types too.
+  nested: { stuff: { type: String, lowercase: true, trim: true } },
+})
+
+// Compile model from schema
+const Products = mongoose.model("Products", ProductsSchema);
+
+module.exports = Products;
