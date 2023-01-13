7c91e8989ac3b7e5c8ea68b253c171913a6c8d4d
Calvin Goodale <calvin.goodale@gmail.com>
2023-01-12 23:26:41 -0500

    Added Mongoose schema

--- server/db.js
+++ server/db.js
@@ -15,16 +15,37 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 const Schema = mongoose.Schema;
 
 const ProductsSchema = new Schema({
+  id: Number,
+  campus: String,
   name: String,
-  binary: Buffer,
-  living: Boolean,
-  updated: { type: Date, default: Date.now() },
-  age: { type: Number, min: 18, max: 65, required: true },
-  mixed: Schema.Types.Mixed,
-  _someId: Schema.Types.ObjectId,
-  array: [],
-  ofString: [String], // You can also have an array of each of the other types too.
-  nested: { stuff: { type: String, lowercase: true, trim: true } },
+  slogan: String,
+  description: String,
+  category: String,
+  default_price: String,
+  created_at: Date,
+  updated_at: Date,
+  features: [
+    {
+      feature: String,
+      value: String
+    }
+  ],
+  results: [
+    {
+      style_id: Number,
+      name: String,
+      original_price: String,
+      sale_price: String,
+      default: Boolean,
+      photos: [
+        {
+          thumbnail_url: String,
+          url: String,
+        }
+      ]
+    }
+  ],
+  related: []
 })
 
 // Compile model from schema
