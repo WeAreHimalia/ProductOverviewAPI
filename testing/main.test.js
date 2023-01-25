634e11cbd86b4234cd6d6f00b1ddc5c45e2e592f
Calvin Goodale <calvin.goodale@gmail.com>
2023-01-24 23:09:59 -0500

    Now querying end of database for optimization testing

--- testing/main.test.js
+++ testing/main.test.js
@@ -24,11 +24,11 @@ describe('GET /products/id', () => {
 
 describe('GET /products/id', () => {
   it('should return the correct statusCode and data for the requested product_id', async () => {
-    const res = await request(server).get('/products/1');
+    const res = await request(server).get('/products/1000011');
     expect(res.statusCode).toBe(200);
-    expect(res.body.product_id).toBe(1);
-    expect(res.body.name).toBe('Camo Onesie');
-    expect(res.body.features.length).toBe(2);
+    expect(res.body.product_id).toBe(1000011);
+    expect(res.body.name).toBe('Evangeline Shoes');
+    expect(res.body.features.length).toBe(1);
   });
 });
 
@@ -52,11 +52,11 @@ describe('GET /products/id CastError ', () => {
 
 describe('GET /products/id/styles', () => {
   it('should return the correct statusCode and data for the requested product_id', async () => {
-    const res = await request(server).get('/products/1/styles');
-    expect(res.body.results.length).toBe(6);
-    expect(res.body.results[0].style_id).toBe(1);
-    expect(res.body.results[0].photos.length).toBe(6);
-    expect(res.body.results[0].skus[1].size).toBe('XS');
+    const res = await request(server).get('/products/1000011/styles');
+    expect(res.body.results.length).toBe(5);
+    expect(res.body.results[0].style_id).toBe(1958098);
+    expect(res.body.results[0].photos.length).toBe(4);
+    expect(res.body.results[0].skus[11323863].size).toBe('7');
   });
 });
 
@@ -76,12 +76,20 @@ describe('GET /products/id/styles CastError ', () => {
   });
 });
 
+describe('GET /products/id/styles with no styles', () => {
+  it('should append the dummy style data if the product does not have any styles', async () => {
+    const res = await request(server).get('/products/1000010/styles');
+    expect(res.body.results.length).toBe(1);
+    expect(res.body.results[0].style_id).toBe(999999999);
+    expect(res.body.results[0].skus[999999999].size).toBe('Xtra M');
+  })
+})
 /******************** RELATED ********************/
 
 describe('GET /products/id/related', () => {
   it('should return the correct statusCode and related products data for the requested product_id', async () => {
-    const res = await request(server).get('/products/1/related');
-    expect(res.body).toEqual([2, 3, 8, 7]);
+    const res = await request(server).get('/products/1000011/related');
+    expect(res.body).toEqual([275004, 93556, 125885, 166656, 875619, 592637]);
   });
 });
 
@@ -124,6 +132,14 @@ describe('POST /cart with incorrect data format', () => {
 });
 
 describe('GET /cart', () => {
+  it('should return the correct statusCode and data for the requested product_id', async () => {
+    const res = await request(server).get('/cart');
+    expect(res.statusCode).toBe(200);
+    expect(res.body[res.body.length - 1].sku_id).toBe(12345);
+  });
+});
+
+describe('GET /cart/sdfd', () => {
   it('should return the correct statusCode and data for the requested product_id', async () => {
     const res = await request(server).get('/cart');
     expect(res.statusCode).toBe(200);
