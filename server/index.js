651291914012476d555f9c2d77c6b4daa63f69d8
Calvin Goodale <calvin.goodale@gmail.com>
2023-02-11 15:00:18 -0500

    Fixed index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const { Cart, Product } = require('./db.js');
 // const db2 = require('./db2.js');
-const { Cart, Product } = require('./db.js');
-// const db2 = require('./db2.js');
 const app = express();
 const port = 3030;
 
