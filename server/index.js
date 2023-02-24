67e4fbfe979318e3020dcbb1804c21be3eb1efdc
Calvin Goodale <calvin.goodale@gmail.com>
2023-02-23 23:43:19 -0500

    fixed...

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Cart, Product } = require('./db.js');
 // const db2 = require('./db2.js');
 const app = express();
-const port = 3000;
+const port = 3030;
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
